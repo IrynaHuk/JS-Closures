957b7fbcb5f9238130c4c18f9df231f793b60f71
IrynaHuk <132898409+IrynaHuk@users.noreply.github.com>
2023-11-08 10:03:40 +0000

    Initial commit

---
+++ src/Weather.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Weather(props) {
+  function farenheitTemperature() {
+    let temperature = (props.temperature * 9) / 5 + 32;
+    return Math.round(temperature);
+  }
+
+  return (
+    <div>
+      The temperature in {props.city} is {props.temperature}°C and{" "}
+      {farenheitTemperature()}°F
+    </div>
+  );
+}
